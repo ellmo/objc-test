c88db6e00d2b44d8483a6e9c68a7dfcc8bb19259
ellmo <ellmunzai@gmail.com>
2014-04-22 17:00:59 +0200

    added XYPoint class

---
+++ prog1/XYPoint.h
@@ -0,0 +1,17 @@
+//
+//  XYPoint.h
+//  prog1
+//
+//  Created by Jakub Żuchowski on 22.04.2014.
+//  Copyright (c) 2014 Jakub Żuchowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYPoint : NSObject
+
+@property int x, y;
+
+-(void) setX: (int) xVal andY: (int) yVal;
+
+@end
---
+++ prog1/XYPoint.m
@@ -0,0 +1,21 @@
+//
+//  XYPoint.m
+//  prog1
+//
+//  Created by Jakub Żuchowski on 22.04.2014.
+//  Copyright (c) 2014 Jakub Żuchowski. All rights reserved.
+//
+
+#import "XYPoint.h"
+
+@implementation XYPoint
+
+@synthesize x, y;
+
+-(void)setX:(int)xVal andY:(int)yVal
+{
+    x = xVal;
+    y = yVal;
+}
+
+@end
