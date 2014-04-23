145ee52e09e1bd417ec3b7f0d69ff89d3e712ce3
ellmo <ellmunzai@gmail.com>
2014-04-23 23:25:09 +0200

    and now removed that nonsense

--- prog1/main.m
+++ prog1/main.m
@@ -7,16 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "GlobalThingieChanger.h"
 
-int gSomeGlobalThingie = 0;
 
 int main (int argc, const char *argv[]) {
     @autoreleasepool {
-        GlobalThingieChanger *frank = [GlobalThingieChanger new];
-        [frank add:3];
-        NSLog(@"global thingie = %d", gSomeGlobalThingie);
-        NSLog(@"global thingie in the instance = %@", [frank toString]);
     }
     return 0;
 }
